cf38a7c1a8026228cfff5e9b47b04b6c7604a7ab
Alex Crawford <efm32gg11b820@code.acrawford.com>
2018-05-05 16:17:06 -0700

    build: update shell derivation and rustfmt config
    Cleans up the syntax a little bit and removes the rustfmt preview.

---
+++ shell.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {
+  overlays = [ (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)) ];
+};
+
+stdenv.mkDerivation {
+  name = "efm32gg11b820";
+  buildInputs = [
+    rustChannels.nightly.rust
+  ];
+}
